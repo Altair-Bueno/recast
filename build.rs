27e92c4bfdcea6e526446f1d541ac7cf58581a7c
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-12-31 20:44:11 +0100

    new: Generate releases on tag creation

--- build.rs
+++ build.rs
@@ -2,21 +2,17 @@ use clap::CommandFactory;
 use clap_complete::generate_to;
 use clap_complete::Shell;
 
-use std::env;
 use std::error::Error;
 
 include!("src/cli.rs");
 
+const COMPLETITIONS_DIR: &str = "target/completitions";
+
 fn gen_completitons() -> Result<(), Box<dyn Error>> {
-    let outdir = match env::var_os("OUT_DIR") {
-        None => return Ok(()),
-        Some(outdir) => outdir,
-    };
+    _ = std::fs::create_dir(COMPLETITIONS_DIR);
     let mut cmd = Config::command();
     for shell in Shell::value_variants() {
-        let path = generate_to(*shell, &mut cmd, "recast", outdir.clone())?;
-        // cargo build 2>&1 >/dev/null | sed -En 's/^.*(RECAST.+)="(.*)"$/\1=\2/gp'
-        // RECAST_bash=/path/to/file
+        let path = generate_to(*shell, &mut cmd, "recast", COMPLETITIONS_DIR)?;
         println!("cargo:warning=RECAST_{shell}={path:?}");
     }
     Ok(())
